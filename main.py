3ab13788b6dcda41f61543a3e9ab8872753b2b55
Suren <surbhas4@gmail.com>
2018-10-17 23:19:07 -0500

    added basic code

--- main.py
+++ main.py
@@ -3,43 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-    
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }
-        </style>
-    </head>
-    <body>
-      <!-- create your form here -->
-      <form action="" method ="post">
-            <label>Rotated by:
-                <input type= "textarea" name= "text"/>
-            </label>
-
-      </form>
-    </body>
-</html>
-
-"""
 @app.route("/")
 def index():
-    
-    
-    #return "hello World"
+    return "hello World"
 
 app.run()   
